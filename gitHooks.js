52d9ecca6745027b8ea1fed26c2ae50f3888f8a8
zheng <383455526@qq.com>
2019-07-17 14:01:26 +0800

    master commit

--- gitHooks.js
+++ gitHooks.js
@@ -1,1 +1,24 @@
-console.log(process)>>"log.txt"
+const simpleGit = require('simple-git/promise')();
+const util = require('util');
+
+function gitMerge(from, to){
+  simpleGit.mergeFromTo(from, to)
+}
+
+(
+  async function(){
+    const branchResult = await Promise.all([simpleGit.branchLocal(),simpleGit.branch()]);
+    let branchSummaryLocal = branchResult[0]
+    let branchSummaryAll = branchResult[1]
+  
+    /** master 分支改变不做处理 */
+    // if(branchSummaryLocal.current === "master"){
+    //   return
+    // }
+
+    /** 合并当前分支到 92Test */
+    gitMerge('master', '92Test')
+    console.log(branchResult)
+  }
+)()
+
